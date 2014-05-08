c36b530db2492bdeb3e165a4af17003e8b0d9bc8
kiiita <kitter2011@gmail.com>
2014-05-08 22:08:57 +0900

    シークレットトークンの生成

--- sample_app/config/initializers/secret_token.rb
+++ sample_app/config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '9b125d55ddffc3fda299373ae21e86949ec3d54324932ea9e91497180f3ba6c1a9f950c449db9c8ef7406edc5e0feb3dbfb0219094455479065c8633cc823da8'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # User the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+# SampleApp::Application.config.secret_key_base = '9b125d55ddffc3fda299373ae21e86949ec3d54324932ea9e91497180f3ba6c1a9f950c449db9c8ef7406edc5e0feb3dbfb0219094455479065c8633cc823da8'
+SampleApp::Application.config.secret_key_base = secure_token
